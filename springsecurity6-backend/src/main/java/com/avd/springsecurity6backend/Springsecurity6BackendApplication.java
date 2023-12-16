0acafbfc4ebc65b957cb5be8ddeb9e3bf4c149dc
avd <avd-web@outlook.com>
2023-12-16 20:23:40 +0100

    Initial commit

---
+++ springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/Springsecurity6BackendApplication.java
@@ -0,0 +1,13 @@
+package com.avd.springsecurity6backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurity6BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurity6BackendApplication.class, args);
+	}
+
+}
---
+++ springsecurity6-backend/src/test/java/com/avd/springsecurity6backend/Springsecurity6BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avd.springsecurity6backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurity6BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
