b640fd09c128a2bead76bdd0d05a3395445ed9b7
Arjan <avd-web@outlook.com>
2023-12-19 23:34:49 +0100

    first basic authentication test completed

--- springsecurity6-backend/src/test/java/com/avd/springsecurity6backend/SpringSecurity6Tests.java
+++ springsecurity6-backend/src/test/java/com/avd/springsecurity6backend/SpringSecurity6Tests.java
@@ -1,6 +1,8 @@
 package com.avd.springsecurity6backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -9,6 +11,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 //@ContextConfiguration(classes = YourWebSecurityConfigClass.class)
 class SpringSecurity6Tests {
 
@@ -17,13 +20,14 @@ class SpringSecurity6Tests {
     public void winTest() {
     }
 
+    @Autowired
     private MockMvc mockMvc;
 
     //TEST AUTHENTICATION
     @Test
-    @WithMockUser(username = "avd", password = "pass")
+    @WithMockUser(username = "user", password = "99f4a362-a79a-4d5b-8672-56c3d8632d19")
     public void testAuthentication() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/login"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
