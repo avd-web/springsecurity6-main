cc95ec2110ef70dbe9417d235b37e52b91ae3413
Arjan <avd-web@outlook.com>
2023-12-20 23:13:09 +0100

    _user table in database

--- springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/user/User.java
+++ springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/user/User.java
@@ -1,10 +1,33 @@
 package com.avd.springsecurity6backend.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User implements UserDetailsService {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
