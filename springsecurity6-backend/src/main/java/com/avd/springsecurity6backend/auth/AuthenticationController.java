859bec986f245e71d4984328eecd0b7bc4ea1e45
avd <avd-web@outlook.com>
2023-12-23 16:27:37 +0100

    Added AuthenticationResponse and, RegisterRequest and, AuthenticationRequest.

--- springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/auth/AuthenticationController.java
+++ springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/auth/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.avd.springsecurity6backend.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("/authentication")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ){
         //
