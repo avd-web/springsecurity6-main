bfe5222710358101434449d4444caa9985906bb3
avd <avd-web@outlook.com>
2023-12-23 12:01:43 +0100

    add generateToken method and build Jwts token out of extraClaims and userDetails

--- springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/config/JwtService.java
+++ springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/config/JwtService.java
@@ -2,11 +2,15 @@ package com.avd.springsecurity6backend.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -15,12 +19,26 @@ public class JwtService {
     private static final String SECRET_KEY = "a9a6b8615f8c6f8737a1c45f98dd928b796a6d91751267b23a10b8f9869c9127";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
