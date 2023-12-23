9f587455e8aed97508156ea36c741c22aff22c57
avd <avd-web@outlook.com>
2023-12-23 11:48:33 +0100

    add in JwtService implementation and add SECRET_KEY, add getSignInKey, add extractAllClaims, add extractClaim,

--- springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/config/JwtService.java
+++ springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/config/JwtService.java
@@ -1,10 +1,40 @@
 package com.avd.springsecurity6backend.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "a9a6b8615f8c6f8737a1c45f98dd928b796a6d91751267b23a10b8f9869c9127";
+
     public String extractUsername(String token) {
         return null;
     }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
