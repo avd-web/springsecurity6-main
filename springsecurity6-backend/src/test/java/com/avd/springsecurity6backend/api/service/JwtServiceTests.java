dac310d1d5cb3c00b950e19a0600ebd39e66e187
Arjan <avd-web@outlook.com>
2024-01-31 14:07:12 +0100

    Added all methods in JwtServiceTests and Refactored3

--- springsecurity6-backend/src/test/java/com/avd/springsecurity6backend/api/service/JwtServiceTests.java
+++ springsecurity6-backend/src/test/java/com/avd/springsecurity6backend/api/service/JwtServiceTests.java
@@ -1,39 +1,31 @@
 package com.avd.springsecurity6backend.api.service;
 
 import com.avd.springsecurity6backend.api.TestUtil;
-import com.avd.springsecurity6backend.auth.RegisterRequest;
 import com.avd.springsecurity6backend.config.JwtService;
-import com.avd.springsecurity6backend.user.Role;
-import org.junit.jupiter.api.BeforeAll;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import com.avd.springsecurity6backend.user.User;
+import io.jsonwebtoken.Claims;
 
-import java.util.Collections;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@RequiredArgsConstructor
 public class JwtServiceTests {
 
     private User mockUser;
     private String username;
 
-
     @BeforeEach
     private void setupUser() {
-
         mockUser = TestUtil.createMockUser1();
         username = mockUser.getUsername();
-
     }
 
     @Autowired
@@ -41,62 +33,60 @@ public class JwtServiceTests {
 
     @Test
     public void testExtractUsername() {
+        String token = jwtService.generateToken(mockUser);
+        String extractedUsername = jwtService.extractUsername(token);
 
+        assertNotNull(extractedUsername);
+        assertEquals(username, extractedUsername);
     }
 
     @Test
     public void testExtractClaim() {
+        String token = jwtService.generateToken(mockUser);
+        String extractedUsername = jwtService.extractClaim(token, Claims::getSubject);
+
+        assertNotNull(extractedUsername);
+        assertEquals(username, extractedUsername);
 
     }
 
     @Test
     public void testGenerateToken() {
-
-
         String token = jwtService.generateToken(mockUser);
-        //ALSO ADD OVERLOADED GENERATE TOKEN FUNCTION?
+        //Also add the overload testGenerateToken method?
 
         assertNotNull(token);
         assertEquals(username, jwtService.extractUsername(token));
-
     }
 
     @Test
     public void testGenerateRefreshToken() {
+        String refreshToken = jwtService.generateRefreshToken(mockUser);
 
+        assertNotNull(refreshToken);
+        assertEquals(username, jwtService.extractUsername(refreshToken));
     }
 
     @Test
-    public void testIsTokenExpired() {
+    public void testIsTokenValid() {
+        String token = jwtService.generateToken(mockUser);
+        boolean isValid = jwtService.isTokenValid(token, mockUser);
 
+        assertTrue(isValid);
     }
 
-
+//    //This is how to test a private method.
 //    @Test
-//    public void testIsTokenValid() {
-//        String username = "testUser";
-//        String password = "testPassword";
-//        UserDetails userDetails = User.withDefaultPasswordEncoder()
-//                .username(username)
-//                .password(password)
-//                .roles("USER")
-//                .build();
+//    public void testIsTokenValid() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+//        String token = jwtService.generateToken(mockUser);
 //
-//        String token = jwtService.generateToken(userDetails);
-//        assertTrue(jwtService.isTokenValid(token, userDetails));
+//        // Use reflection to invoke the private method
+//        Method method = JwtService.class.getDeclaredMethod("isTokenExpired", String.class);
+//        method.setAccessible(true);
+//        boolean isExpired = (Boolean) method.invoke(jwtService, token);
+//
+//        assertFalse(isExpired);
+//
 //    }
 
-//    @Test
-//    public void testIsTokenExpired() {
-//        String username = "testUser";
-//        String password = "testPassword";
-//        UserDetails userDetails = User.withDefaultPasswordEncoder()
-//                .username(username)
-//                .password(password)
-//                .roles("USER")
-//                .build();
-//
-//        String token = jwtService.generateToken(userDetails);
-//        assertFalse(jwtService.isTokenExpired(token));
-//    }
 }
