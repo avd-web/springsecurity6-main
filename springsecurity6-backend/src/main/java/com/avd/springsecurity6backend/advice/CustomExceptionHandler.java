9cc629584fcb6db0176083ba1fffcb79f0c6c5cd
avd <avd-web@outlook.com>
2024-02-18 15:05:13 +0100

    Error-handling startup 2 added more in CustomExceptionHandler class

--- springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/advice/CustomExceptionHandler.java
+++ springsecurity6-backend/src/main/java/com/avd/springsecurity6backend/advice/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.avd.springsecurity6backend.advice;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -7,29 +8,44 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.nio.file.AccessDeniedException;
+import java.security.SignatureException;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+
     // Bad Credential : Authentication Failure : 401
     // Access denied : Authorization ERROR : 403
-    // Invalid JWT Signature : 403
-    // Token Expired
+    // Invalid JWT Signature : 403 : (JWT) SignatureException
+    // Token Expired : ExpiredJwtException
+
+    // Todo: add seeder for users to be able to test auth.
+    // Todo: write failing tests for register and authentication errors.
 
     @ExceptionHandler(Exception.class)
-    public ProblemDetail handleSecurityException(Exception ex){
+    public ProblemDetail handleSecurityException(Exception ex) {
 
         ProblemDetail errorDetail = null;
 
-          if (ex instanceof BadCredentialsException){
-              errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), ex.getMessage());
-              errorDetail.setProperty("access_denied_reason", "Authentication Failure");
-          }
+        if (ex instanceof BadCredentialsException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "Authentication Failure");
+        }
 
-          if (ex instanceof AccessDeniedException){
-              errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
-              errorDetail.setProperty("access_denied_reason", "not-authorized");
-          }
-    return errorDetail;
+        if (ex instanceof AccessDeniedException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "not-authorized");
+        }
+
+        if (ex instanceof SignatureException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "JWT Signature not valid");
+        }
+
+        if (ex instanceof ExpiredJwtException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), ex.getMessage());
+            errorDetail.setProperty("access_denied_reason", "JWT token already expired");
+        }
+        return errorDetail;
     }
 }
